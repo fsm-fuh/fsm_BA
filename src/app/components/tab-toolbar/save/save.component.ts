900430c425f114da0a803622e4826ec000ef69d0
Alex <92081744+alexkolbfleisch@users.noreply.github.com>
2025-12-08 17:35:38 +0100

    [FPWGT-63] add PNML support for Petri net serialization and parsing (#16)
    * [FPWGT-63] add PNML support for Petri net serialization and parsing
    
    * [FPWGT-63] improve null handling and UI updates

--- src/app/components/tab-toolbar/save/save.component.ts
+++ src/app/components/tab-toolbar/save/save.component.ts
@@ -2,17 +2,18 @@ import { Component, inject } from '@angular/core';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { PetriNetSavingService } from '../../../services/petri-net-saving.service';
+import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
 
 @Component({
     selector: 'app-save',
-    imports: [MatIcon, MatButton],
+    imports: [MatIcon, MatButton, MatMenu, MatMenuItem, MatMenuTrigger],
     templateUrl: './save.component.html',
     styleUrl: './save.component.css',
 })
 export class SaveComponent {
     private _petriNetSavingService = inject(PetriNetSavingService);
 
-    protected onSave() {
-        this._petriNetSavingService.savePetriNet();
+    protected onSave(format: 'json' | 'pnml') {
+        this._petriNetSavingService.savePetriNet(format);
     }
 }
