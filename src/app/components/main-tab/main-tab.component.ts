b6c78c39591bbc185a3eb9c07d62dbcb56a860c0
Ben Leitzbach <benleitzbach1@gmail.com>
2025-12-30 12:21:28 +0100

    Implemented Panning Service to PN Tab

--- src/app/components/main-tab/main-tab.component.ts
+++ src/app/components/main-tab/main-tab.component.ts
@@ -47,7 +47,7 @@ export class MainTabComponent {
     private _playService: PlayService = inject(PlayService);
     private readonly _tabs: Tab[] = [Tab.DRAW, Tab.PLAY, Tab.REACHABILITY_GRAPH, Tab.PROCESS_NET];
 
-    selectedIndex = Tab.DRAW; // Select which tab to show by default
+    selectedIndex = Tab.PROCESS_NET; // Select which tab to show by default
 
     onTabChange(event: MatTabChangeEvent) {
         this._tabStateService.switchTo(this._tabs[event.index]);
