d9cc378e355643014bff2f3b8f2c4659ac40f317
Ben Leitzbach <benleitzbach1@gmail.com>
2026-01-05 20:54:42 +0100

    Update process-net.component.ts

--- src/app/components/tab-toolbar/process-net/process-net.component.ts
+++ src/app/components/tab-toolbar/process-net/process-net.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit, inject, DestroyRef } from '@angular/core';
+import { Component, OnInit, inject, DestroyRef } from '@angular/core';
 import { ProcessNetDisplayComponent } from './process-net-display/process-net-display.component';
 import { ProcessNetDrawDisplayComponent } from './process-net-draw-display/process-net-draw-display';
 import { DisplayService } from '../../../services/display.service';
@@ -6,7 +6,6 @@ import { SourcePetriNetService } from '../../../services/source-petri-net.servic
 import { SerializationService } from '../../../services/serialization.service';
 import { ParserService } from '../../../services/parser.service';
 import { Diagram } from '../../../classes/diagram/diagram';
-import { Subscription } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
@@ -17,23 +16,18 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
     styleUrl: './process-net.component.css',
     providers: [DisplayService],
 })
-export class ProcessNetComponent implements OnInit, OnDestroy {
+export class ProcessNetComponent implements OnInit {
     private displayService = inject(DisplayService);
     private sourcePetriNetService = inject(SourcePetriNetService);
     private serializationService = inject(SerializationService);
     private parserService = inject(ParserService);
     private destroyRef = inject(DestroyRef);
-    private sub?: Subscription;
 
     ngOnInit(): void {
         this.pushCloneToLocalDisplay(this.sourcePetriNetService.getCurrentSourceNet());
-        this.sub = this.sourcePetriNetService.sourceNet$
+        this.sourcePetriNetService.sourceNet$
             .pipe(takeUntilDestroyed(this.destroyRef))
             .subscribe((net) => this.pushCloneToLocalDisplay(net));
-    }
-
-    ngOnDestroy(): void {
-        this.sub?.unsubscribe();
     }
 
     private pushCloneToLocalDisplay(net: Diagram | null): void {
